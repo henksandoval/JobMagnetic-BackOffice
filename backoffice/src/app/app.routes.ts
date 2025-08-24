ee75b56ce0bd9299c3ef2dd6888ee1bf5a7a94fd
Henk Sandoval <henk.sandoval@outlook.com>
2025-08-24 23:19:21 +0200

    feat: add DashboardComponent with routing and initial layout for dashboard view

--- backoffice/src/app/app.routes.ts
+++ backoffice/src/app/app.routes.ts
@@ -4,6 +4,17 @@ import {LayoutComponent} from './layout/layout.component';
 export const routes: Routes = [
   {
     path: '',
-    component: LayoutComponent
-  }
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent)
+      }
+    ]
+  },
 ];
